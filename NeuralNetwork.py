5ff11bd0cb7fb1273dd4acdde3358791627b9543
bmkrutgers <44555410+bmkrutgers@users.noreply.github.com>
2019-11-09 11:00:49 -0500

    Added the ways to classify and read data

---
+++ NeuralNetwork.py
@@ -0,0 +1,12 @@
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import make_classification
+
+
+X, y = make_classification(n_samples=1000, n_features=4,n_informative=2, n_redundant=0,random_state=0, shuffle=False)
+
+clf = RandomForestClassifier(n_estimators=100, max_depth=2,random_state=0)
+
+clf.fit(X, y)
+
+print(clf.predict([[0, 0, 0, 0]]))
